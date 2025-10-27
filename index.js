e61b1826950c3ab1c2b15d5a4d76490ee202170e
Rafsanr <rafsan.ratul26@compscihigh.org>
2025-10-27 19:19:14 +0000

    completed

--- index.js
+++ index.js
@@ -4,22 +4,30 @@
 // MAKE SURE TO SELECT API AND NOT SDK & put "Web" as your platform
 // Save your API key in a variable below
 
+let key = "JXrN3XBIK6nCvc1msOBiWzNf2CGY2g94"
 
 // Task 2: Endpoint Identification
 // Under GIPHY API, find the GIF & Sticker endpoints
 // Find the endpoint that lets you search for GIFs 
 // Save the base and path in a variable below
-
-
+let Url = "https://api.giphy.com/v1/gifs/search"
 // Task 3: Building full request URL
 // Find the request (query) parameters to build the full request URL to search for 1 raccoon GIFs
 // Save it in a variable below
-
+let Url2 = `https://api.giphy.com/v1/gifs/search?api_key=&q=raccoon`
 
 // Task 4: Making the API call
 // Define the function to fetch the data and console log the url key
 
+async function call(u){
+    let response = await fetch(u)
+    let data = await response.json()
+    return data
+}
 
+call(Url2).then(data => {
+    console.log(data)
+})
 
 // Call the function to test it worked
 
@@ -31,6 +39,14 @@
 
 // Define and call the function that prints just the sticker URL
 
+let Url3=`https://api.giphy.com/v1/sticker/random?limit=1&api_key=${key}`
 
+async function stick(s){
+    let response = await fetch(Url3);
+    let data = await response2.json();
+    console.log(data.data[0])
+}
 
-
+call(Url3).then(data => {
+    console.log(data)
+}
